234c5f3964cef210f1bbbae84fdc22789aa6a719
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-10 13:32:03 +0530

    Initial Push from local

---
+++ LineLength.cs
@@ -0,0 +1,12 @@
+﻿
+namespace LineComparisonD3
+{
+    public class LineLength
+    {
+        public static double LengthCalculation(int x1, int y1, int x2,int y2 )
+        {
+            double lengthOfLine = Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2));
+            return lengthOfLine;
+        }
+    }
+}
